f688074cad2416e01804f5ede9fd4ed67f3602d9
Karol Bohdanowicz <bohunek5@gmail.com>
2026-07-01 23:46:46 +0200

    Fix static export redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import { redirect } from 'next/navigation';
+"use client";
+
+import { useEffect } from 'react';
 
 export default function RootPage() {
-  redirect('/pl');
+  useEffect(() => {
+    window.location.replace('/SKUTERY-GIZYCKO/pl');
+  }, []);
+  
+  return null;
 }
