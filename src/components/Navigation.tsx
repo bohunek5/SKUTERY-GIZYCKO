ffb05ea98986cb6713d252b4f19c361879472c94
Karol Bohdanowicz <bohunek5@gmail.com>
2026-07-02 16:59:29 +0200

    Fix weather widget to show wind on mobile top bar and add scale for smaller screens

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -39,7 +39,7 @@ export default function Navigation() {
         </div>
 
         <div className={styles.mobileTopBarControls}>
-          <WeatherWidget compact={true} />
+          <WeatherWidget />
           <ThemeToggle compact={true} />
           <LanguageSwitcher compact={true} />
         </div>
