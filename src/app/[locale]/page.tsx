b022f8c1cc4d94fa904b359d2e7291e535574224
Karol Bohdanowicz <bohunek5@gmail.com>
2026-07-02 12:22:17 +0200

    feat: add HowItWorks and About sections to homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,8 @@
 import { setRequestLocale } from 'next-intl/server';
 import Hero from '@/components/Hero';
 import Fleet from '@/components/Fleet';
+import HowItWorks from '@/components/HowItWorks';
+import About from '@/components/About';
 
 export default async function Home({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
@@ -11,6 +13,8 @@ export default async function Home({ params }: { params: Promise<{ locale: strin
     <main>
       <Hero />
       <Fleet />
+      <HowItWorks />
+      <About />
     </main>
   );
 }
