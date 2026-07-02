4edfb7db981422986da3cab55f40982e37928771
Karol Bohdanowicz <bohunek5@gmail.com>
2026-07-02 14:25:58 +0200

    Update hero video to the correct one and regenerate all posters

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -15,7 +15,7 @@ export default async function Home({ params }: { params: Promise<{ locale: strin
       <Hero 
         title={t('title')} 
         subtitle={t('subtitle')} 
-        videoSrc="/SKUTERY-GIZYCKO/videos/hero.mov?v=4" 
+        videoSrc="/SKUTERY-GIZYCKO/videos/hero.mp4?v=5" 
         showArrowDown={true}
       />
       <Fleet />
